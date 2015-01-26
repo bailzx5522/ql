4d16429b610f8a70909e29308d848b82429e35bf
root <root@vm10-128-16-42.ksc.com>
2015-01-26 18:16:46 +0800

    blz

--- ql/strategy/sr_strategy.py
+++ ql/strategy/sr_strategy.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import math
 
-from ql.lib.plot import Plot
+#from ql.lib.plot import Plot
 from ql.strategy import Strategy
 from ql.db.sql_api import get_engine
 
@@ -17,21 +17,36 @@ class MMStrategy(Strategy):
         self.data = data
 
     def get_rs(self):
-        src = self.data['close']
-        print src
+        df = self.data.copy()
+        max = df.max(index=1)['close']
+        min = df.min(index=1)['close']
+        result = []
+        rows = len(df)
+        for index, row in df.iterrows():
+            close = row['close']
+            begin = index-11 if index>11 else 0
+            end = index+11 if index<rows-11 else rows-11
+            if close == df['close'][begin:end].max():
+                result.append(close)
+        sp = pd.Series(result, dtype=float)
 
-
-
-
-        #df = self.data.copy()
-        #grouped = df.groupby('date')
-        #day_high = grouped['high'].max()
-        #day_low = grouped['low'].min()
-        #day_high.apply(self._select, args=(day_high,))
-
-        #support = self._select(day_low)
-        resistance = self._select(day_high)
-
+        last = 0
+        ret = []
+        ordered = sp.order()
+        print ordered
+        for i,v in ordered.iteritems():
+            if last == 0:
+                last = v
+                bias = v*0.01
+                ret.append(v)
+            elif v - last <= bias:
+                pass
+            elif v - last > bias:
+                ret.append(v)
+            last = v
+            bias = v*0.01
+        print pd.Series(ret, dtype=float)
+                
     def generate_signal(self):
         pass
 
